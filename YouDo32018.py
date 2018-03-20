992e5e65890dd6962d466fff04e1896b9491e24a
Five-Sn <32499540+Five-Sn@users.noreply.github.com>
2018-03-20 17:32:48 -0500

    A bunch of small activities

---
+++ YouDo32018.py
@@ -0,0 +1,29 @@
+# You Do Round 1
+favorite_animals = ["tiger", "llama", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalpaca"]
+winning_lottery_numbers = [13, 42, 89, 0.1]
+print favorite_animals
+print winning_lottery_numbers
+
+# You Do Round 2
+old_list = ["A", "T", "M", "I", "P", "H", "L", "Y", "U", "N", "T", "G", "O"]
+new_list = []
+new_list.append(old_list[4])
+new_list.append(old_list[7])
+new_list.append(old_list[10])
+new_list.append(old_list[5])
+new_list.append(old_list[12])
+new_list.append(old_list[9])
+print new_list
+
+# You Do Round 3
+another_list = [1, 3, 5]
+another_list[1] = another_list[1] * 5
+print another_list
+
+# You Do Round 4
+students = {"Billy": 16, "Miguel": 18, "Barney": 19, "Quintin": 2000}
+print students
+
+# You Do Round 5
+savings_accounts = {"Jolene": 500.00, "America": 575.00, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalpaca": 1212098523720342000.00}
+print savings_accounts
